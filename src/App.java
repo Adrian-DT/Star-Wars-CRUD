8505a62cbd730d47e59f2398776a4aee1d1dace2
Adrian Delgado <adriandt_work@outlook.com>
2025-07-17 12:14:46 +0200

    Realización de lectura de información de la base de datos mediante filtros pasados por parámetros

--- src/App.java
+++ src/App.java
@@ -1,16 +1,43 @@
-import model.*;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+import model.Personaje;
+import model.PersonajeDAO;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        PersonajeDAO personajeDAO = new PersonajeDAO();
 
+        int option = Integer.parseInt(JOptionPane.showInputDialog(
+            "1 - Insertar Personaje\n" +
+            "2 - Listar Personajes\n" +
+            "3 - Actualizar Personaje\n" +
+            "4 - Eliminar Personaje\n" +
+            "5 - Buscar Personaje\n" +
+            "6 - Salir"));
 
-        PersonajeDAO model = new PersonajeDAO();
+        switch (option) {
+            case 1:
+                if (personajeDAO.addPersonaje(new Personaje("Darth Vader", "Imperio", "Lord Sith"))) {
+                    JOptionPane.showMessageDialog(null, "Personaje creado con éxito");
+                } else {
+                    JOptionPane.showMessageDialog(null, "No se pudo crear el personaje");
+                }
+                break;
+            case 2:
+                ArrayList<String> data = new ArrayList<String>();
+                data.add("Imperio");
 
-        if(model.addPersonaje(new Personaje("Luke Skywalker", "Rebel Alliance", "Jedi"))) {
-            System.out.println("Personaje creado con exito");
-        } else {
-            System.out.println("No se pudo crear el personaje");
+                ArrayList<Personaje> listaPersonajes = personajeDAO.getPersonaje("faccion", data);
+
+                for (Personaje personaje : listaPersonajes) {
+                    System.out.println("\nNombre: " + personaje.getNombre() + "\nFaccion: " + personaje.getFaccion() + "\nDescripcion: " + personaje.getDescripcion());
+                }
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Opción no válida");
+                break;
         }
-
     }
 }
+
--- src/model/PersonajeDAO.java
+++ src/model/PersonajeDAO.java
@@ -2,11 +2,14 @@ package model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class PersonajeDAO {
 
     private PreparedStatement preparedStatement;
+    private ResultSet resultSet;
 
     public boolean addPersonaje(Personaje personaje) {
         boolean state = false;
@@ -28,12 +31,10 @@ public class PersonajeDAO {
                 preparedStatement.setString(3, personaje.getDescripcion());
 
                 // Comprobamos el estado de la ejecución de la sentencia SQL
-                // Modificamos el del bool para conocer el resultado de la ejecución
+                // Ejecutamos la sentencia y almacenamos el resultado para modificar el boolean para conocer el resultado de la ejecución
                 int resultado = preparedStatement.executeUpdate();
                 state = resultado > 0 ? true : false;
 
-                // Ejecutamos la sentencia
-                preparedStatement.executeUpdate();
                 // Personaje personajeJDBC = new Personaje(connect);
                 // state = personajeJDBC.addPersonaje(personaje);
             } else {
@@ -55,4 +56,86 @@ public class PersonajeDAO {
         return state;
     }
 
+    public ArrayList<Personaje> getPersonaje(String filter, ArrayList<String> data) {
+        ArrayList<Personaje> listaPersonajes = new ArrayList<Personaje>();
+        Personaje personaje;
+
+
+        Connection connect = null;
+
+        try {
+            // Obtenemos la conexión con la base de datos
+            connect = ConnectionPool.getInstance().getConnection();
+
+            if (connect != null) {
+                
+                String sql = "";
+                // En función del filtro recibido por argumento, generamos una sentencia SQL u otra
+                switch (filter) {
+                    case "id":
+                        sql = "SELECT * FROM personaje WHERE id = ?";
+                        break;
+                    case "nombre":
+                    // Usamos REGEXP ? para que la busqueda sea case insensitive y obtengamos coincidencias
+                        sql = "SELECT * FROM personaje WHERE nombre REGEXP ?";
+                        break;
+                    case "faccion":
+                        sql = "SELECT * FROM personaje WHERE faccion REGEXP ?";
+                        break;
+                    case "descripcion":
+                        sql = "SELECT * FROM personaje WHERE descripcion REGEXP ?";
+                        break;
+                    default:
+                        sql = "SELECT * FROM personaje";
+                        break;
+                }
+                // Preparamos la sentencia
+                preparedStatement = connect.prepareStatement(sql);
+
+                // En función del filter indicamos el value de la sentencia SQL
+                switch (filter) {
+                    case "id":
+                        preparedStatement.setInt(1, Integer.parseInt(data.get(0)));
+                        break;
+                    case "nombre":
+                        preparedStatement.setString(1, data.get(0));
+                        break;
+                    case "faccion":
+                        preparedStatement.setString(1, data.get(0));
+                        break;
+                    case "descripcion":
+                        preparedStatement.setString(1, data.get(0));
+                        break;
+                }
+
+                // Ejecutamos la sentencia y obtenemos el resultado
+                resultSet = preparedStatement.executeQuery();
+
+                // Recorremos el resultSet y obtenemos los datos
+                while (resultSet.next()) {
+                    personaje = new Personaje();
+                    personaje.setId(resultSet.getInt("id"));
+                    personaje.setNombre(resultSet.getString("nombre"));
+                    personaje.setFaccion(resultSet.getString("faccion"));
+                    personaje.setDescripcion(resultSet.getString("descripcion"));
+                    listaPersonajes.add(personaje);
+                }
+
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Cerramos la conexión con la base de datos en un finally para que se ejecute en cualquier caso
+            try {
+                // Pasamos el objeto de tipo conection a la clase ConnectionPool para cerrarla
+                ConnectionPool.getInstance().closeConnection(connect);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        return listaPersonajes;
+    }
 }
